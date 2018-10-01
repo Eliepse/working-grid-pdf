11ea053527547633d6a469c65c469457ed89e9c3
Élie Meignan <contact@eliepse.fr>
2018-10-01 13:29:17 +0200

    refactor: Add more exemples

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,6 +4,8 @@ The format is based on [Keep a Changelog](http://keepachangelog.com/en/1.0.0/)
 and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.html).
 
 ## Unreleased
+### Added
+- Added more exemples
 
 ## 0.2.1 - 2018-10-01
 ### Changed
--- public/index.php
+++ public/index.php
@@ -7,10 +7,20 @@ require '../vendor/autoload.php';
 
 function getStrokes(string $filename): array { return json_decode(file_get_contents("../resources/samples/strokes/$filename.json"), true); }
 
-$sheet = new PDFWorkingGrid("Exemple grid", true, 9);
+$sheet = new PDFWorkingGrid("Exemple grid", true, 9, 10);
 
-$sheet->addCharacter(new Character("中", getStrokes("0")));
-$sheet->addCharacter(new Character("国", getStrokes("1")));
+$sheet->addCharacter(new Character("中", getStrokes("zhong")));
+$sheet->addCharacter(new Character("国", getStrokes("guo")));
+$sheet->addCharacter(new Character("一", getStrokes("yi")));
+$sheet->addCharacter(new Character("二", getStrokes("er")));
+$sheet->addCharacter(new Character("三", getStrokes("san")));
+$sheet->addCharacter(new Character("四", getStrokes("si")));
+$sheet->addCharacter(new Character("五", getStrokes("wu")));
+$sheet->addCharacter(new Character("六", getStrokes("liu")));
+$sheet->addCharacter(new Character("七", getStrokes("qi")));
+$sheet->addCharacter(new Character("八", getStrokes("ba")));
+$sheet->addCharacter(new Character("九", getStrokes("jiu")));
+$sheet->addCharacter(new Character("十", getStrokes("shi")));
 
 /** @noinspection PhpUnhandledExceptionInspection */
 $sheet->output();
