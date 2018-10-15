3f4121c229356386ff85417b5c6ca7ec2e84134e
Eliepse <contact@eliepse.fr>
2018-10-15 12:28:26 +0200

    feat: add trait to allow attributes filling

---
+++ src/FillAttributes.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: micro
+ * Date: 14/10/2018
+ * Time: 12:22
+ */
+
+namespace Eliepse\WorkingGrid;
+
+
+trait FillAttributes
+{
+
+    /**
+     * @param array $attributes
+     * @return FillAttributes
+     */
+    public function fill(array $attributes): self
+    {
+        $attrs = get_class_vars(self::class);
+
+        foreach ($attributes as $name => $value) {
+
+            // Check if the attribute exists and the given value is not null
+            if (array_key_exists($name, $attrs) && !is_null($value))
+                $this->$name = $value;
+
+        }
+
+        return $this;
+    }
+
+}
