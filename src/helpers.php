ddc831f5432ab9e73656aade3f9d80c04379de42
Eliepse <contact@eliepse.fr>
2018-10-21 21:39:30 +0200

    fix: add namespace to helpers to prevent fatal error from functions multi-declarion

--- src/helpers.php
+++ src/helpers.php
@@ -1,52 +1,57 @@
 <?php
 
-use Eliepse\WorkingGrid\Exception\ViewNotFoundException;
 
-/**
- * @param string $viewPath
- * @param array $data
- * @return string
- * @throws ViewNotFoundException
- */
-function view(string $viewPath, array $data = []): string
-{
-    preg_match("/\.([a-zA-Z0-9-_]+)$/", $viewPath, $matches);
+namespace Eliepse\WorkingGrid {
 
-    $view_name = $matches[1];
-
-    $filepath = mberegi_replace("\.", DIRECTORY_SEPARATOR, $viewPath);
-
-    if (file_exists($filepath))
-        throw new ViewNotFoundException($view_name, $viewPath);
-
-    ob_start() && extract($data);
-
-    /** @noinspection PhpIncludeInspection */
-    require resources_path("views/$filepath.php");
-
-    return ob_get_clean();
-
-}
-
-function base_path(string $path = ""): string
-{
-    return (__DIR__ . "/../$path");
-}
-
-function resources_path(string $path = ""): string
-{
-    return base_path("resources/$path");
-}
-
-if (!function_exists("array_contains")) {
+    use Eliepse\WorkingGrid\Exception\ViewNotFoundException;
+    
     /**
-     * Check if an array contains an item at least once
-     * @param $array
-     * @param $item
-     * @return bool
+     * @param string $viewPath
+     * @param array $data
+     * @return string
+     * @throws ViewNotFoundException
      */
-    function array_contains($array, $item): bool
+    function view(string $viewPath, array $data = []): string
     {
-        return boolval(count(array_keys($array, $item, true)));
+        preg_match("/\.([a-zA-Z0-9-_]+)$/", $viewPath, $matches);
+
+        $view_name = $matches[1];
+
+        $filepath = mberegi_replace("\.", DIRECTORY_SEPARATOR, $viewPath);
+
+        if (file_exists($filepath))
+            throw new ViewNotFoundException($view_name, $viewPath);
+
+        ob_start() && extract($data);
+
+        /** @noinspection PhpIncludeInspection */
+        require resources_path("views/$filepath.php");
+
+        return ob_get_clean();
+
     }
+
+    function base_path(string $path = ""): string
+    {
+        return (__DIR__ . "/../$path");
+    }
+
+    function resources_path(string $path = ""): string
+    {
+        return base_path("resources/$path");
+    }
+
+    if (!function_exists("array_contains")) {
+        /**
+         * Check if an array contains an item at least once
+         * @param $array
+         * @param $item
+         * @return bool
+         */
+        function array_contains($array, $item): bool
+        {
+            return boolval(count(array_keys($array, $item, true)));
+        }
+    }
+
 }
