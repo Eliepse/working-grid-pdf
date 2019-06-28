c9088589c3e5bf43b84395f2913d1e490c12d1ec
Élie Meignan <contact@eliepse.fr>
2019-06-28 17:16:38 +0800

    refactor: simplify example

--- src/Template/Examples/StrokesTemplate.php
+++ src/Template/Examples/StrokesTemplate.php
@@ -3,34 +3,11 @@
 
 namespace Eliepse\WorkingGrid\Template\Examples;
 
-use Eliepse\WorkingGrid\PageInfo;
 use Eliepse\WorkingGrid\Template\CustomizableFooter;
 use Eliepse\WorkingGrid\Template\CustomizableHeader;
-use Eliepse\WorkingGrid\Template\Template;
 use Eliepse\WorkingGrid\Template\WithDrawingTutorial;
-use Mpdf\Mpdf;
 
-class StrokesTemplate extends Template implements CustomizableHeader, CustomizableFooter, WithDrawingTutorial
+class StrokesTemplate extends DefaultTemplate implements CustomizableHeader, CustomizableFooter, WithDrawingTutorial
 {
-
-    public $title = "Exemple grid 中文";
-
-    public $header_height = 20;
-
-    public $footer_height = 12;
-
-
-    public function header(Mpdf $pdf, PageInfo $infos): void
-    {
-        $pdf->SetFontSize(16);
-        $pdf->Cell(210, 0, $this->title, false, false, 'C', 0, 0, 0, '', 'T', 'T');
-    }
-
-
-    public function footer(Mpdf $pdf, PageInfo $infos): void
-    {
-        $pdf->SetFontSize(10);
-        $pdf->Cell(210, $this->footer_height, "{$infos->getIndex()} / {$infos->getTotal()}", false, false, 'C', 0, 0, 0, '', 'T', 'B');
-    }
-
+    public $title = "Strokes Exemple grid 中文";
 }
