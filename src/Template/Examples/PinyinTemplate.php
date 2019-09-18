b1a6de41ca3a9ddb28630058ea575b2210f3becf
Eliepse <contact@eliepse.fr>
2019-09-18 14:25:39 +0200

    fix: correct exemples

--- src/Template/Examples/PinyinTemplate.php
+++ src/Template/Examples/PinyinTemplate.php
@@ -8,7 +8,7 @@ use Eliepse\WorkingGrid\Template\CustomizableHeader;
 use Eliepse\WorkingGrid\Template\WithDrawingTutorial;
 use Eliepse\WorkingGrid\Template\WithPinyin;
 
-class PinyinTemplate extends DefaultTemplate implements CustomizableHeader, CustomizableFooter, WithPinyin
+class PinyinTemplate extends DefaultTemplate implements WithPinyin, WithDrawingTutorial
 {
     public $title = "Pinyin Exemple grid 中文";
 }
--- src/Template/Examples/StrokesTemplate.php
+++ src/Template/Examples/StrokesTemplate.php
@@ -7,7 +7,7 @@ use Eliepse\WorkingGrid\Template\CustomizableFooter;
 use Eliepse\WorkingGrid\Template\CustomizableHeader;
 use Eliepse\WorkingGrid\Template\WithDrawingTutorial;
 
-class StrokesTemplate extends DefaultTemplate implements CustomizableHeader, CustomizableFooter, WithDrawingTutorial
+class StrokesTemplate extends DefaultTemplate implements WithDrawingTutorial
 {
     public $title = "Strokes Exemple grid 中文";
 }
